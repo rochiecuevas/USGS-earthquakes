198a3e43549d7eb5e2d2514ed3c4803e7c45c26f
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-19 04:36:28 -0800

    attempting to add legend

--- Resources/logic.js
+++ Resources/logic.js
@@ -40,10 +40,10 @@ function createFeatures(earthquakeData) {
             // Use the customColor function to assign colours to the circles based on magnitudes of the earthquakes
             color: customColor(earthquakeData[i].properties.mag),
             fillColor: customColor(earthquakeData[i].properties.mag),
-            fillOpacity: 0.75,
+            fillOpacity: 1,
 
             // Radius of each circle is based on the magnitude of the earthquake
-            radius: earthquakeData[i].properties.mag 
+            radius: earthquakeData[i].properties.mag * 3
         }).bindPopup("<h3>" + earthquakeData[i].properties.place + "</h3><hr><p>Magnitude: " + earthquakeData[i].properties.mag + "<br>Depth: " + earthquakeData[i].geometry.coordinates[2] + "km<br>Significance: " + earthquakeData[i].properties.sig + "<br>Time: " + new Date(earthquakeData[i].properties.time)+ "</p>");
 
         earthquakeMarkers.push(marker);
@@ -63,13 +63,14 @@ function createBoundaries(boundaryData) {
 
     // Create a polygon for each tectonic plate
     // (1) Create an empty list
-    var bounds = [];
+    var latlon = [];
 
     // (2) Loop through the boundaryData array
     for (var j = 0; j < boundaryData.length; j ++) {
-        bounds.push(boundaryData[0].geometry.coordinates);
+        latlon.push(boundaryData[0].geometry.coordinates);
     };
-    console.log(bounds);
+    console.log(latlon);
+
 
 };
 
@@ -84,17 +85,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
     
-  var lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.light",
+    id: "mapbox.satellite",
     accessToken: API_KEY
   });
 
     // Define a baseMap that contains the two layers
     var baseMap = {
         "Street Map": streetMap,
-        "Light Map": lightMap
+        "Satellite Map": satelliteMap
     }
 
     // Create an overlayMap that contains the earthquake geoJSON layer
@@ -109,7 +110,7 @@ function createMap(earthquakes) {
     var map = L.map("map", {
         center: MapCoords,
         zoom: mapZoomLevel,
-        layers: [lightMap, streetMap, earthquakes]
+        layers: [satelliteMap, streetMap, earthquakes]
     });
 
     // Create a layer control to pass the baseMap and the overlayMap and add it to the map
@@ -117,6 +118,34 @@ function createMap(earthquakes) {
         collapsed: false
     });
     control.addTo(map);
+
+    // Create a legend
+    // var legend = L.control({position: "bottomright"});
+    // legend.onAdd = function() {
+    //     var div = L.DomUtil.create("div", "info-legend");
+    //     var categories = [2, 4, 5, 6, 7, 8, 9]
+    //     var colors = ["#ffffb2", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#b10026"];
+    //     var labels = [];
+
+    // var legendInfo = "<h4>Earthquake Magnitude</h4>" +
+    //     "<div class = \"labels\">" + 
+    //     "<div class = \"min\">" + categories[0] + "</div>" +
+    //     "<div class = \"max\">" + categories[categories.length - 1] + "</div>" +
+    //     "</div>";
+    
+    // div.innerHTML = legendInfo;
+
+    // categories.forEach(function(cat, index) {
+    //     labels.push("<li style = \"background-color: " + colors[index] + "\"></li>");
+    // });
+
+    // div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+
+    // return div;
+    // }
+
+    // legend.addTo(map);
+
 };
 
 // Create an API call to the USGS geojson for all-day earthquake records (which are updated every five minutes)
