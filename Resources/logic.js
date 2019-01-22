3d4016760eacc45ec6c1916295ca3065a322ebcc
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-22 10:11:35 -0800

    added tectonic plate data but map not showing it yet

--- Resources/logic.js
+++ Resources/logic.js
@@ -1,3 +1,10 @@
+// Define the URLs
+var platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+
+var earthquakes = new L.layerGroup()
+var plates = new L.layerGroup();
+
 // Define a function that will run once per earthquake (object) in the response.features array
 function createFeatures(earthquakeData) {
     console.log(earthquakeData[0]); 
@@ -50,32 +57,45 @@ function createFeatures(earthquakeData) {
     };
     console.log(earthquakeMarkers);
 
-    // Create an earthquake layerGroup containing the objects in the features array
-    var earthquakes = L.layerGroup(earthquakeMarkers);
+    // (3) Create an earthquake layerGroup containing the objects in the features array
+    earthquakes = L.layerGroup(earthquakeMarkers);
+    
+    // Perform a request to get the information from the plates URL (JSON format)
+d3.json(platesURL, function(error, data){
+    if (error) throw error;
 
-    // Send the earthquakes layer to the createMap function
-    createMap(earthquakes);
-};
+    // Send the data.features (array) to the createBoundaries function
+    var boundaryData = data.features;
+    // createBoundaries(boundaryData);
 
-// Define a function that will run once per plate (object) in the data.features array
-function createBoundaries(boundaryData) {
-    console.log(boundaryData[0].geometry);
-
-    // Create a polygon for each tectonic plate
-    // (1) Create an empty list
     var latlon = [];
 
     // (2) Loop through the boundaryData array
     for (var j = 0; j < boundaryData.length; j ++) {
-        latlon.push(boundaryData[0].geometry.coordinates);
+        latlon.push(boundaryData[j].geometry.coordinates);
     };
     console.log(latlon);
 
+    var plateBoundaries = L.polyline(latlon, {
+        color: "red"
+    });
 
+    plates = L.layerGroup(plateBoundaries);
+
+    console.log(plateBoundaries);
+    return plates;
+});
+
+    // Send the earthquakes layer to the createMap function
+    createMap(earthquakes, plates);
 };
 
+
+
 // Define a function that creates a map
-function createMap(earthquakes) {
+function createMap(earthquakes, plates) {
+    console.log(earthquakes);
+    console.log(plates);
     
     // Define a street layer and a satellite layer
     var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -83,24 +103,33 @@ function createMap(earthquakes) {
     maxZoom: 18,
     id: "mapbox.streets",
     accessToken: API_KEY
-  });
+    });
     
-  var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.satellite",
-    accessToken: API_KEY
-  });
+    var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+    var darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+    });
 
     // Define a baseMap that contains the two layers
     var baseMap = {
         "Street Map": streetMap,
-        "Satellite Map": satelliteMap
+        "Satellite Map": satelliteMap,
+        "Dark Map": darkMap
     }
 
     // Create an overlayMap that contains the earthquake geoJSON layer
     var overlayMap = {
-        "Earthquakes Today": earthquakes
+        "Earthquakes Today": earthquakes,
+        "Tectonic Plates": plates
     };
 
     // Create the map, centred somewhere near 20ºN and 10ºE
@@ -110,7 +139,7 @@ function createMap(earthquakes) {
     var map = L.map("map", {
         center: MapCoords,
         zoom: mapZoomLevel,
-        layers: [satelliteMap, streetMap, earthquakes]
+        layers: [satelliteMap, darkMap, streetMap, earthquakes,plates]
     });
 
     // Create a layer control to pass the baseMap and the overlayMap and add it to the map
@@ -120,54 +149,37 @@ function createMap(earthquakes) {
     control.addTo(map);
 
     // Create a legend
-    // var legend = L.control({position: "bottomright"});
-    // legend.onAdd = function() {
-    //     var div = L.DomUtil.create("div", "info-legend");
-    //     var categories = [2, 4, 5, 6, 7, 8, 9]
-    //     var colors = ["#ffffb2", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#b10026"];
-    //     var labels = [];
+    var legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info-legend");
+        var categories = ["<2", "4", "5", "6", "7", "8", ">9"]
+        var colors = ["#ffffb2", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#b10026"];
+        var labels = [];
 
-    // var legendInfo = "<h4>Earthquake Magnitude</h4>" +
-    //     "<div class = \"labels\">" + 
-    //     "<div class = \"min\">" + categories[0] + "</div>" +
-    //     "<div class = \"max\">" + categories[categories.length - 1] + "</div>" +
-    //     "</div>";
+    var legendInfo = "<h4>Legend: <br> Earthquake Magnitude</h4>";
     
-    // div.innerHTML = legendInfo;
+    div.innerHTML = legendInfo;
 
-    // categories.forEach(function(cat, index) {
-    //     labels.push("<li style = \"background-color: " + colors[index] + "\"></li>");
-    // });
+    categories.forEach(function(cat, index) {
+        labels.push("<span style = \"background-color: " + colors[index] + "\">" + " " + cat + " " + "</span>");
+    });
 
-    // div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    div.innerHTML += "<p style = 'text-align: center;'>" + labels.join(" ") + "</p>";
 
-    // return div;
-    // }
+    return div;
+    }
 
-    // legend.addTo(map);
+    legend.addTo(map);
 
 };
 
 // Create an API call to the USGS geojson for all-day earthquake records (which are updated every five minutes)
-var API = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 // Perform a GET request to the USGS API endpoint
-d3.json(API, function(error, response){
+d3.json(quakeURL, function(error, response){
     if (error) throw error;
 
     // Send the response.features (array) to the createFeatures function
     var earthquakeData = response.features
     createFeatures(earthquakeData); // response.features = earthquakeData
-});
-
-// Get data about tectonic plate boundaries
-var platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
-
-// Perform a request to get the information from the plates URL (JSON format)
-d3.json(platesURL, function(error, data){
-    if (error) throw error;
-
-    // Send the data.features (array) to the createBoundaries function
-    var boundaryData = data.features;
-    createBoundaries(boundaryData);
 });
