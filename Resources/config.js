b3b014edc085bf527e2b8943739b0c12a267d78b
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-22 10:11:05 -0800

    updated API key

--- Resources/config.js
+++ Resources/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoicm9jaGllY3VldmFzIiwiYSI6ImNqcXR1eGlwdjBkcmMzeHJ4YXQ5dHh4aWoifQ.GO47iuimfy3AQnpPdmoNRQ";
+var API_KEY = "pk.eyJ1Ijoicm9jaGllY3VldmFzIiwiYSI6ImNqcXR1eGlwdjBkcmMzeHJ4YXQ5dHh4aWoifQ.GO47iuimfy3AQnpPdmoNRQ";
